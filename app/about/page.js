fa9aaaf22f76b4e7c7e5011d14049b0210592467
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-26 12:13:39 -0500

    Update with new information

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function About () {
   return (
     <>
@@ -6,10 +8,10 @@ export default function About () {
           <h1 className="text-4xl font-bold">About Me</h1>
         </div>
         <div>
-          <p className="mb-3">Hi! I'm currently pursuing a master's degree in Computational Science and Engineering at Harvard, and I graduated from UT Austin in Chemical Engineering. I'm currently exploring the software engineering and web development space, and I've developed some personal projects along the way as a means to learn new things.</p>
-          <p className="mb-3">Through high school and college, I treated coding more of as an interest; I completed the Computer Science certificate at UT Austin and casually went through problems from Project Euler and Leetcode. In my junior year of college, I realized that I wanted to pursue a career in software engineering. I gained experience through industry internships, academic research, and personal projects, and I aim to continue learning and growing through my master's program.</p>
-          <p className="mb-3">My plan through the master's program has been to essentially take all of the standard CS courses that I didn't in my undergrad. So far, I've taken courses on data structures and algorithms, artificial intelligence, and systems development, and I'm currently taking courses on graph theory, computer graphics, high performance computing, and computer networking.</p>
-          <p className="mb-5"> I've been learning web development on my own, and I've developed a few side projects along the way (including this website!). I come from a Python background with a bit of C++ splashed in, and have been gradually immersing myself into the JavaScript ecosystem. I started from static HTML/CSS/JS, then learned dynamic web development with Flask, and finally moved into React via Next.js. Since then, I've also started using TypeScript and TailwindCSS, as well as learning C++ at a more in-depth level on the side. I've recently discovered an interest in graphics as well, leading me to build my first graphics-based project in an implementation of Conway's Game of Life. Some of my more prominent projects can be found on the Projects page, and all of my projects can be found on my GitHub, linked in the footer.</p>
+          <p className="mb-3">Hi! I'm currently working as a SWE at Bloomberg in NYC as a recent graduate, with a passion for backend development. I got my B.S. from UT Austin in Chemical Engineering and my M.S. in Computational Science and Engineering at Harvard. I'm always exploring the software engineering and web development space to learn new things, and I've developed some small personal projects along the way to grow those skills.</p>
+          <p className="mb-3">Through high school and college, coding was more of an interest to me; I would casually go through problems from repositories like Project Euler or Leetcode. In my junior year of college, I realized that I wanted to fully pursue a career in CS, and decided to pursue opportunities to realize that dream. I took internships at Genentech and Amazon, did academic research, and made some personal projects to help pivot my skillset into the CS area, and I'm happy to say that it's paid off.</p>
+          <p className="mb-3">In terms of technologies, I learned Python and Java as my first two languages, then eventually learned JS and React for web development projects. Then through internships and coursework, I grew fairly proficient in C and C++. I'm currently looking to continue to learn other backend-focused languages such as Go and Rust.</p>
+          <p className="mb-3">Some of my projects are linked in the <Link href="/projects" className="underline">Projects</Link> tab, and the rest are on my GitHub. They aren't super large projects, but they've helped me gain proficiency with different frameworks and general principles.</p>
         </div>
       </div>
     </>
--- app/projects/ProjectExamples.js
+++ app/projects/ProjectExamples.js
@@ -16,27 +16,25 @@ export default function ProjectExamples () {
     <div className='flex flex-wrap mb-3'>
       { projectData.map((project, index) => {
         return (
-          <>
-            <div key={index}>
-              <div className='flex flex-col w-full md:w-1/2 p-2'>
-                <div className='mb-3 hover:scale-105'>
-                  <a href={project.url} target='_blank' rel='noreferrer'>
-                    <img src={project.imageUrl} alt={project.name} className='w-full' />
-                  </a>
-                </div>
-                <div>
-                  <a href={project.url} target='_blank' rel='noreferrer' className='mb-3'>
-                    <h2 className='font-bold'>{project.name}</h2>
-                  </a>
-                </div>
-                <div>
-                  <p>{project.description}</p>
-                </div>
+          <div key={index} className='w-full md:w-1/2'>
+            <div className='flex flex-col w-full p-2'>
+              <div className='mb-3 hover:scale-105'>
+                <a href={project.url} target='_blank' rel='noreferrer'>
+                  <img src={project.imageUrl} alt={project.name} className='w-full' />
+                </a>
               </div>
-              {index % 2 === 0 && index !== projectData.length - 1 && <hr className='w-full mb-3 border-0 bg-gray-300 h-px block md:hidden' />}
-              {index % 2 === 1 && index !== projectData.length - 1 && <hr className='w-full mb-3 border-0 bg-gray-300 h-px' />}
+              <div>
+                <a href={project.url} target='_blank' rel='noreferrer' className='mb-3'>
+                  <h2 className='font-bold'>{project.name}</h2>
+                </a>
+              </div>
+              <div>
+                <p>{project.description}</p>
+              </div>
             </div>
-          </>
+            {/* {index % 2 === 0 && index !== projectData.length - 1 && <hr className='w-full mb-3 border-0 bg-gray-300 h-px block md:hidden' />}
+            {index % 2 === 1 && index !== projectData.length - 1 && <hr className='w-full mb-3 border-0 bg-gray-300 h-px' />} */}
+          </div>
         )
       })}
     </div>      
