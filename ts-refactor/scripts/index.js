99eea9ea6471356cdf3f69e0eaf052ef7c4398e8
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-27 23:39:42 -0500

    Add about page

--- ts-refactor/scripts/index.js
+++ ts-refactor/scripts/index.js
@@ -1,8 +1,3 @@
-var button = document.getElementById('test-button');
-button.addEventListener('click', function () {
-    console.log('Button clicked!');
-});
-
 function loadTemplate(url, elementId) {
     fetch(url)
         .then(response => response.text())
