88d5e9421012e6ccadd8e146800945ca0b3e4604
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-30 14:54:26 -0500

    Try to fix header and footer loading

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,35 @@
-function loadTemplate(url, elementId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        })
-        .catch(error => console.error('Error loading template', error));
+function loadHeader() {
+    let headerContainer = document.getElementById('header-container');
+    const header = `
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/resume.pdf" target="_blank">Resume</a>
+    `;
+
+    headerContainer.innerHTML = header;
 }
 
-loadTemplate('../components/header.html', 'header-container');
-loadTemplate('../components/footer.html', 'footer-container');
+function loadFooter() {
+    let footerContainer = document.getElementById('footer-container');
+    const footer = `
+        <p id="contact-title">Contact me:</p>
+        <div id="footer-links">
+        <a href='mailto:michaelzhan2001@gmail.com'>
+            <i class="fa-solid fa-envelope"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/michaelzhan1/">
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://www.github.com/michaelzhan1/">
+            <i class="fa-brands fa-github"></i>
+        </a>
+        </div>
+        <p id="copyright">© 2025 Michael Zhan</p>
+    `;
+
+    footerContainer.innerHTML = footer;
+}
+
+loadHeader();
+loadFooter();
