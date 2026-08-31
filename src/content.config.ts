a56392a54853de1105451aebb0354064d0a7c228
Michael Zhan <michaelzhan2001@gmail.com>
2026-08-31 00:17:24 -0400

    formats

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,22 @@
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
-import { defineCollection } from 'astro:content';
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
   }),
-})
+});
 
 const projects = defineCollection({
-  loader: glob({ base: './src/content/projects', pattern: '**/*.md' }),
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     link: z.url(),
   }),
-})
+});
 
 export const collections = { blog, projects };
